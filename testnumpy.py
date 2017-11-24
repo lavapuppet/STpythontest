19ba7982fcdd02df66671185a0d921620ebd4a7c
SurajSting <suraj.murali.9300@student.uu.se>
2017-11-24 16:31:20 +0100

    some dot tests

--- testnumpy.py
+++ testnumpy.py
@@ -1,4 +1,19 @@
 import Numpy as np
+import unittest
+
+class TestLinAlg(unittest.TestCase):
+    """
+
+    """
+
+    def setUp(self):
+        self.array_1 = [[1, 0], [0, 1]]
+        self.array_2 = [[4, 1], [2, 2]]
+
+    def test_dot(self):
+        (result_array) = np.dot(self.array_1, self.array_2)
+        self.assertEqual(result_array , [[4, 1], [2, 2]])
 
 
-
+if __name__ == '__main__':
+    unittest.main()
