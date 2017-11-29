3212529df9986b0ffef2c8e85d3b77e4c75dddd4
SurajSting <suraj.murali.9300@student.uu.se>
2017-11-29 17:10:50 +0100

    Added raises exception

--- testnumpy.py
+++ testnumpy.py
@@ -2,18 +2,6 @@ import numpy as np
 import unittest
 
 class TestLinAlg(unittest.TestCase):
-    """
-<<<<<<< HEAD
-    NP.DOT()
-    testing55
-=======
-    NP.DOT() function
-# >>>>>>> 9c3e2e099d489829acd321758644fd26059dd104
-
-JOHAN testing...
-JOHAN testing 2
-    """
-
     def setUp(self):
         """ Basic Identity test """
         self.array_1 = [[1, 0], [0, 1]]
@@ -35,36 +23,58 @@ JOHAN testing 2
         """ Perpendicular Test [1]"""
         self.array_per_1 = [2.0 , 1.0, 4.0]
         self.array_per_2 = [1.0 , -1.0, -0.25]
+
+    """
+        Testing numpy.linalg.dot() function
+    """
+    
+    def test_dot_raises(self):
+        with self.assertRaises(ValueError):
+            actual = np.dot([2, 2, 3], [2, 1])
         
+#        try:
+ #           self.assertFalse(True)
+  #      except ValueError:
+   #         pass
         
+    
     def test_dot_iden(self):
-        result_array = np.dot(self.array_1, self.array_2)
-        self.assertTrue((result_array == [[4, 1], [2, 2]]).all())
+        actual = np.dot(self.array_1, self.array_2)
+        expected = [[4, 1], [2, 2]]
+        self.assertTrue((actual == expected).all())
 
     def test_dot_zero(self):
-        result_zero = np.dot(self.array_zero, self.array_2)
-        self.assertTrue((result_zero == 0).all())
+        actual = np.dot(self.array_zero, self.array_2)
+        expected = 0
+        self.assertTrue((actual == expected).all())
         
     def test_dot_com(self):
-        result_array_left = np.dot(self.array_com_1, self.array_com_2)
-        result_array_right = np.dot(self.array_com_2, self.array_com_1)
-        self.assertTrue((result_array_left == result_array_right).all())
-
-    def test_dot_lin(self):
-        result_array_lin_left = np.dot(self.array_com_1,(np.dot(self.scalar, self.array_com_2)+ self.array_com_3))
-        print(result_array_lin_left)
-        result_array_lin_right = self.scalar * (np.dot(self.array_com_1, self.array_com_2) + (np.dot(self.array_com_1 ,self.array_com_3)))
-        print(result_array_lin_right)
-        self.assertTrue((result_array_lin_left == result_array_lin_right).all())
+        actual = np.dot(self.array_com_1, self.array_com_2)
+        expected = np.dot(self.array_com_2, self.array_com_1)
+        self.assertTrue((actual == expected).all())
 
     def test_dot_square(self):
-        result_array_square = np.dot(self.array_2, self.array_2)
-        self.assertTrue((result_array_square == [[18, 6], [12, 6]]).all())
+        actual = np.dot(self.array_2, self.array_2)
+        expected = [[18, 6], [12, 6]]
+        self.assertTrue((actual == expected).all())
 
-    def test_dot_perpendicular(self):
-        result_array_perpendicular = np.dot(self.array_per_1, self.array_per_2)
-        self.assertTrue((result_array_perpendicular == 0).all())
+    def test_dot_perpendicular(self):        
+        actual = np.dot(self.array_per_1, self.array_per_2)
+        expected = 0
+        self.assertTrue((actual == expected).all())
+
+    """
+        Testing numpy.linalg.vdot() function
+    """
+
+    def test_vdot_iden(self):
+        actual = np.vdot(self.array_1, self.array_2)
+        expected = 6
+        self.assertTrue((actual == expected).all())
+
     
+    
+        
 
 if __name__ == '__main__':
     unittest.main()
