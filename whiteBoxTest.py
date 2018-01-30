4e9f9c24276b80502a4f1fcbbab0223922cb43e4
Regina Kelly <rkelly1@tcd.ie>
2018-01-30 11:04:26 +0100

    almost finished

--- whiteBoxTest.py
+++ whiteBoxTest.py
@@ -28,7 +28,7 @@ class WhiteBox(unittest.TestCase):
         self.array_empty = np.empty([2,2])
 
         """Linear Test [1]"""
-        self.array_com_3 = [12.4, -1.7, 3.15]
+        self.array_com_3 = ([12.4],[3.15])
         self.scalar = np.array([0.22])
         self.sint = np.array([7,7]) 
         self.sfloat = np.array([6.6,6.6])
@@ -57,31 +57,52 @@ class WhiteBox(unittest.TestCase):
         expected1 = np.array([[19, 22], [43, 50]])
         self.assertTrue((actual == expected1).all())
 
-    def test_multi_ndim_01(self):
+    def test_multi_ndim_01(self): #cost2 15-18
         actual = linpy.multi_dot([self.sint,self.array_1, self.array_2])
         expected = np.array([434,504])
         self.assertTrue((actual == expected).all())
         
-    def test_multi_ndim_00(self):
+    def test_multi_ndim_00(self): #cost1 14-19
         actual = linpy.multi_dot([self.array_5,self.array_1, self.array_2])
         expected = np.array([[329,382],[224,260],[873,1014]])
         self.assertTrue((actual == expected).all())
 
-    def test_multi_ndim_end1(self):
+    def test_multi_ndim_end1(self): #cost1 14-18
         actual = linpy.multi_dot([self.array_1, self.array_2,self.sint])
         expected = np.array([287,651])
         self.assertTrue((actual == expected).all())
     
     
-    def test_multi_ndim_11(self):
-        actual = linpy.multi_dot([self.sint,self.array_1, self.array_2,self.sfloat])
-        expected = np.array([6190.8])
-        self.assertAlmostEqual(actual, expected)
+    def test_multi_ndim_11(self):# 8-10 - 18
+        actual = linpy.multi_dot([self.sint,self.array_1, self.array_2,self.array_4])
+        expected = np.array([5124,3682,12768])
+        self.assertTrue((actual == expected).all())
         
-    def test_many_ndim_00(self):
+    def test_many_ndim_11(self):#8-10 16-17
         actual = linpy.multi_dot([self.sint,self.array_1, self.array_2,self.array_4,self.array_3,self.scalar])
         expected = np.array([62000.4])
         self.assertAlmostEqual(actual, expected)
+
+    def test_many_ndim_00(self):#8-10 16-19
+        actual = linpy.multi_dot([self.array_1,self.array_2_2_identity, self.array_2,self.array_4,self.array_3])
+        expected = np.array([[12318],[27942]])
+        self.assertTrue((actual == expected).all())
+
+    def test_3_C2_11(self): #cost2 15-17
+        actual = linpy.multi_dot([self.sint,self.array_com_3, self.scalar])
+        expected = np.array([23.947])
+        self.assertTrue((actual == expected).all())
+    
+    def test_3_C1_00(self): #cost1 14-17
+        actual = linpy.multi_dot([self.array_5,self.array_1, self.array_2])
+        expected = np.array([[329,382],[224,260],[873,1014]])
+        self.assertTrue((actual == expected).all())
+
+    def test_3_C2_00(self): #cost2 15-19
+        actual = linpy.multi_dot([self.array_1,self.array_2, self.array_4])
+        expected = np.array([[224,161,558],[508,365,1266]])
+        self.assertTrue((actual == expected).all())
+   
 
 if __name__ == '__main__':
     unittest.main()
